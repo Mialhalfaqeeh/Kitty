659f0b793c525df5621ce2f8c0973ac18c86b25f
milahalfaqeeh22 <mohammedalfaqeeh1405@gmail.com>
2022-12-17 09:42:41 +0300

    Initial Commit

---
+++ Kitty/KittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittyApp.swift
+//  Kitty
+//
+//  Created by Milah Alfaqeeh  on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct KittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
